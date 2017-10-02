7c5b4a9a2690b0e7f79cf6a3817bbb590551335e
Jessica Stokes <hello@jessicastokes.net>
2017-10-01 22:50:28 -0700

    🎮 Move SDL init to main

--- src/main.rs
+++ src/main.rs
@@ -21,72 +21,7 @@ fn main() {
         .subcommand(SubCommand::with_name("test").about("Tests the game controller subsystem"))
         .get_matches();
 
-    match matches.subcommand_name() {
-        Some("test") => {
-            test();
-        }
-        Some("ps2ce") => {
-            let ps2ce_matches = matches.subcommand_matches("ps2ce").unwrap();
-            let device_path = ps2ce_matches.value_of("device").unwrap();
-            ps2ce(&device_path).unwrap();
-        }
-        _ => (),
-    }
-}
-
-fn ps2ce(device_path: &str) -> io::Result<()> {
-    println!("Connecting to PS2 Controller Emulator device at '{}'...", device_path);
-
-    let mut serial = match serial::open(device_path) {
-        Ok(serial) => serial,
-        Err(error) => panic!("failed to open serial device: {}", error)
-    };
-
-    try!(serial.reconfigure(&|settings| {
-        try!(settings.set_baud_rate(serial::Baud9600));
-        settings.set_char_size(serial::Bits8);
-        Ok(())
-    }));
-
-    println!("Connected!");
-    println!("Sending a command!");
-
-    let buf = vec!(
-        0x5A, // DualShock Magic
-
-        // Buttons (0=Pressed)
-        //┌─────────── Left
-        //│┌────────── Down
-        //││┌───────── Right
-        //│││┌──────── Up
-        //││││┌─────── [Start>
-        //│││││┌────── (R3)
-        //││││││┌───── (L3)
-        //│││││││┌──── [Select]
-        0b11111111u8,
-        0b11111111u8,
-        //│││││││└──── [L2]
-        //││││││└───── [R2]
-        //│││││└────── [L1]
-        //││││└─────── [R1]
-        //│││└──────── Triangle
-        //││└───────── Circle
-        //│└────────── Cross
-        //└─────────── Square
-
-        // Sticks
-        0x80, // Right stick X
-        0x80, // Right stick Y
-        0x80, // Left stick X
-        0x80, // Left stick Y
-    );
-
-    try!(serial.write(&buf[..]));
-
-    Ok(())
-}
-
-fn test() {
+    // Initialise SDL2, and the game controller subsystem
     let sdl_context = sdl2::init().unwrap();
     let game_controller_subsystem = sdl_context.game_controller().unwrap();
 
@@ -136,7 +71,83 @@ fn test() {
     println!("(There are {} controllers connected)",
              active_controllers.len());
 
-    // Listen to SDL events!
+    match matches.subcommand_name() {
+        Some("ps2ce") => {
+            let ps2ce_matches = matches.subcommand_matches("ps2ce").unwrap();
+            let device_path = ps2ce_matches.value_of("device").unwrap();
+
+            send_to_ps2_controller_emulator(&device_path,
+                                            sdl_context,
+                                            game_controller_subsystem,
+                                            &mut active_controllers).unwrap();
+        }
+        Some("test") => {
+            print_events(sdl_context,
+                         game_controller_subsystem,
+                         &mut active_controllers);
+        }
+        _ => (),
+    }
+}
+
+fn send_to_ps2_controller_emulator(device_path: &str,
+                                   sdl_context: sdl2::Sdl,
+                                   game_controller_subsystem: sdl2::GameControllerSubsystem,
+                                   active_controllers: &mut HashMap<i32, sdl2::controller::GameController>) -> io::Result<()> {
+    println!("Connecting to PS2 Controller Emulator device at '{}'...", device_path);
+
+    let mut serial = match serial::open(device_path) {
+        Ok(serial) => serial,
+        Err(error) => panic!("failed to open serial device: {}", error)
+    };
+
+    try!(serial.reconfigure(&|settings| {
+        try!(settings.set_baud_rate(serial::Baud9600));
+        settings.set_char_size(serial::Bits8);
+        Ok(())
+    }));
+
+    println!("Connected!");
+
+    let buf = vec!(
+        0x5A, // DualShock Magic
+
+        // Buttons (0=Pressed)
+        //┌─────────── Left
+        //│┌────────── Down
+        //││┌───────── Right
+        //│││┌──────── Up
+        //││││┌─────── [Start>
+        //│││││┌────── (R3)
+        //││││││┌───── (L3)
+        //│││││││┌──── [Select]
+        0b11111111u8,
+        0b11111111u8,
+        //│││││││└──── [L2]
+        //││││││└───── [R2]
+        //│││││└────── [L1]
+        //││││└─────── [R1]
+        //│││└──────── Triangle
+        //││└───────── Circle
+        //│└────────── Cross
+        //└─────────── Square
+
+        // Sticks
+        0x80, // Right stick X
+        0x80, // Right stick Y
+        0x80, // Left stick X
+        0x80, // Left stick Y
+    );
+
+    try!(serial.write(&buf[..]));
+
+    Ok(())
+}
+
+fn print_events(sdl_context: sdl2::Sdl,
+                game_controller_subsystem: sdl2::GameControllerSubsystem,
+                active_controllers: &mut HashMap<i32, sdl2::controller::GameController>) {
+    println!("Printing all controller events...");
     for event in sdl_context.event_pump().unwrap().wait_iter() {
         use sdl2::event::Event;
 
@@ -160,6 +171,21 @@ fn test() {
                 }
             }
 
+            Event::ControllerDeviceRemoved { which, .. } => {
+                println!("{} (#{}): disconnected",
+                         active_controllers[&which].name(),
+                         which);
+                println!("(There are {} controllers connected)",
+                         active_controllers.len() - 1);
+                active_controllers.remove(&which);
+            }
+
+            Event::ControllerDeviceRemapped { which, .. } => {
+                println!("{} (#{}) remapped!",
+                         active_controllers[&which].name(),
+                         which);
+            }
+
             Event::ControllerAxisMotion { which, axis, value, .. } => {
                 println!("{} (#{}): {:?}: {}",
                          active_controllers[&which].name(),
@@ -180,21 +206,6 @@ fn test() {
                          active_controllers[&which].name(),
                          which,
                          button);
-            }
-
-            Event::ControllerDeviceRemoved { which, .. } => {
-                println!("{} (#{}): disconnected",
-                         active_controllers[&which].name(),
-                         which);
-                println!("(There are {} controllers connected)",
-                         active_controllers.len() - 1);
-                active_controllers.remove(&which);
-            }
-
-            Event::ControllerDeviceRemapped { which, .. } => {
-                println!("{} (#{}) remapped!",
-                         active_controllers[&which].name(),
-                         which);
             }
 
             Event::Quit { .. } => break,
