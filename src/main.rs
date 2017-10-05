73420dfc4b5caf8b762dfcf78e38ae6e7ca35e62
Jessica Stokes <hello@jessicastokes.net>
2017-10-04 20:55:41 -0700

    🎮 Split out SDL Manager module

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ extern crate serial;
 #[macro_use]
 extern crate clap;
 use serial::prelude::*;
-use std::collections::HashMap;
 use std::io;
 use std::io::prelude::*;
+
+mod sdl_manager;
+use sdl_manager::SDLManager;
 
 static DUALSHOCK_MAGIC: u8 = 0x5A;
 
@@ -13,72 +15,6 @@ enum ControllerEmulatorPacketType {
     None, // Fallback, just log messages
     SevenByte, // For Johnny Chung Lee's firmware
     TwentyByte, // For Aaron Clovsky's firmware
-}
-
-struct SDLManager {
-    context: sdl2::Sdl,
-    game_controller_subsystem: sdl2::GameControllerSubsystem,
-    active_controllers: HashMap<i32, sdl2::controller::GameController>,
-}
-
-impl SDLManager {
-    fn init() -> SDLManager {
-        // Initialise SDL2, and the game controller subsystem
-        let context = sdl2::init().unwrap();
-        let game_controller_subsystem = context.game_controller().unwrap();
-
-        // Load pre-set controller mappings (note that SDL will still read
-        // others from the SDL_GAMECONTROLLERCONFIG environment variable)
-        let controller_mappings = include_str!(
-            "../vendor/SDL_GameControllerDB/gamecontrollerdb.txt"
-        ).lines()
-            .map(|line| line.trim())
-            .filter(|line| !line.is_empty() && !line.starts_with('#'));
-
-        // Load each mapping individually rather than using load_mappings,
-        // as it turns out doing them together can break without warning
-        // if the file's syntax is ever invalid
-        for mapping in controller_mappings {
-            match game_controller_subsystem.add_mapping(mapping) {
-                Err(error) => panic!("failed to load mapping: {}", error),
-                _ => (),
-            }
-        }
-
-        // Keep track of the controllers we know of
-        let mut active_controllers: HashMap<i32, sdl2::controller::GameController> = HashMap::new();
-
-        // Look into controllers that were already connected at start-up
-        let joystick_count = match game_controller_subsystem.num_joysticks() {
-            Ok(count) => count,
-            Err(error) => panic!("failed to enumerate joysticks: {}", error),
-        };
-
-        for id in 0..joystick_count {
-            if game_controller_subsystem.is_game_controller(id) {
-                match game_controller_subsystem.open(id) {
-                    Ok(controller) => {
-                        let controller_id = &controller.instance_id();
-                        println!("{} (#{}): found", controller.name(), controller_id);
-                        active_controllers.insert(*controller_id, controller);
-                    }
-                    Err(error) => {
-                        println!(
-                            "could not initialise joystick {} as controller: {:?}",
-                            id,
-                            error
-                        )
-                    }
-                }
-            }
-        }
-
-        return SDLManager {
-            context,
-            game_controller_subsystem,
-            active_controllers,
-        };
-    }
 }
 
 fn main() {
---
+++ src/sdl_manager.rs
@@ -0,0 +1,72 @@
+extern crate sdl2;
+use std::collections::HashMap;
+
+// SDL Manager
+// Structure for passing around access to the SDL Subsystems,
+// and central place for setting up defaults
+
+pub struct SDLManager {
+    pub context: sdl2::Sdl,
+    pub game_controller_subsystem: sdl2::GameControllerSubsystem,
+    pub active_controllers: HashMap<i32, sdl2::controller::GameController>,
+}
+
+impl SDLManager {
+    pub fn init() -> SDLManager {
+        // Initialise SDL2, and the game controller subsystem
+        let context = sdl2::init().unwrap();
+        let game_controller_subsystem = context.game_controller().unwrap();
+
+        // Load pre-set controller mappings (note that SDL will still read
+        // others from the SDL_GAMECONTROLLERCONFIG environment variable)
+        let controller_mappings = include_str!(
+            "../vendor/SDL_GameControllerDB/gamecontrollerdb.txt"
+        ).lines()
+            .map(|line| line.trim())
+            .filter(|line| !line.is_empty() && !line.starts_with('#'));
+
+        // Load each mapping individually rather than using load_mappings,
+        // as it turns out doing them together can break without warning
+        // if the file's syntax is ever invalid
+        for mapping in controller_mappings {
+            match game_controller_subsystem.add_mapping(mapping) {
+                Err(error) => panic!("failed to load mapping: {}", error),
+                _ => (),
+            }
+        }
+
+        // Keep track of the controllers we know of
+        let mut active_controllers: HashMap<i32, sdl2::controller::GameController> = HashMap::new();
+
+        // Look into controllers that were already connected at start-up
+        let joystick_count = match game_controller_subsystem.num_joysticks() {
+            Ok(count) => count,
+            Err(error) => panic!("failed to enumerate joysticks: {}", error),
+        };
+
+        for id in 0..joystick_count {
+            if game_controller_subsystem.is_game_controller(id) {
+                match game_controller_subsystem.open(id) {
+                    Ok(controller) => {
+                        let controller_id = &controller.instance_id();
+                        println!("{} (#{}): found", controller.name(), controller_id);
+                        active_controllers.insert(*controller_id, controller);
+                    }
+                    Err(error) => {
+                        println!(
+                            "could not initialise joystick {} as controller: {:?}",
+                            id,
+                            error
+                        )
+                    }
+                }
+            }
+        }
+
+        return SDLManager {
+            context,
+            game_controller_subsystem,
+            active_controllers,
+        };
+    }
+}
