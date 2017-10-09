2b649d118a9052f3eba361bf0547c4657f0999fc
Jessica Stokes <hello@jessicastokes.net>
2017-10-09 16:55:52 -0700

    🍕 Modularise SDLManager

--- src/sdl_manager.rs
+++ src/sdl_manager.rs
@@ -14,10 +14,20 @@ pub struct SDLManager {
 
 impl SDLManager {
     pub fn init() -> SDLManager {
-        // Initialise SDL2, and the game controller subsystem
+        // Initialise SDL2, plus the haptic and game controller subsystems
         let context = sdl2::init().unwrap();
         let haptic_subsystem = context.haptic().unwrap();
         let game_controller_subsystem = context.game_controller().unwrap();
+
+        // Keep track of the controllers we know of
+        let mut active_controllers: HashMap<i32, sdl2::controller::GameController> = HashMap::new();
+
+        let mut sdl_manager = SDLManager {
+            context,
+            game_controller_subsystem,
+            haptic_subsystem,
+            active_controllers,
+        };
 
         // Load pre-set controller mappings (note that SDL will still read
         // others from the SDL_GAMECONTROLLERCONFIG environment variable)
@@ -31,45 +41,41 @@ impl SDLManager {
         // as it turns out doing them together can break without warning
         // if the file's syntax is ever invalid
         for mapping in controller_mappings {
-            match game_controller_subsystem.add_mapping(mapping) {
+            match sdl_manager.game_controller_subsystem.add_mapping(mapping) {
                 Err(error) => panic!("failed to load mapping: {}", error),
                 _ => (),
             }
         }
 
-        // Keep track of the controllers we know of
-        let mut active_controllers: HashMap<i32, sdl2::controller::GameController> = HashMap::new();
-
         // Look into controllers that were already connected at start-up
-        let joystick_count = match game_controller_subsystem.num_joysticks() {
+        sdl_manager.add_available_controllers();
+
+        return sdl_manager;
+    }
+
+    pub fn add_available_controllers(&mut self) {
+        let joystick_count = match self.game_controller_subsystem.num_joysticks() {
             Ok(count) => count,
             Err(error) => panic!("failed to enumerate joysticks: {}", error),
         };
 
-        for id in 0..joystick_count {
-            if game_controller_subsystem.is_game_controller(id) {
-                match game_controller_subsystem.open(id) {
+        for index in 0..joystick_count {
+            if self.game_controller_subsystem.is_game_controller(index) {
+                match self.game_controller_subsystem.open(index) {
                     Ok(controller) => {
                         let controller_id = &controller.instance_id();
                         println!("{} (#{}): found", controller.name(), controller_id);
-                        active_controllers.insert(*controller_id, controller);
+                        self.active_controllers.insert(*controller_id, controller);
                     }
                     Err(error) => {
                         println!(
                             "could not initialise joystick {} as controller: {:?}",
-                            id,
+                            index,
                             error
                         )
                     }
                 }
             }
         }
-
-        return SDLManager {
-            context,
-            game_controller_subsystem,
-            haptic_subsystem,
-            active_controllers,
-        };
     }
 }
