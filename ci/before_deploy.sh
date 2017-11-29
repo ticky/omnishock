94cd8ba6afdf244f2398283d8b23a0673869e8a6
Jessica Stokes <hello@jessicastokes.net>
2017-11-29 13:56:15 +1100

    🏢 Fix Windows deploy

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,10 +17,8 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    # TODO Update this to build the artifacts that matter to you
     cross rustc --bin omnishock --target $TARGET --release -- -C lto
 
-    # TODO Update this to package the right artifacts
     cp target/$TARGET/release/omnishock $stage/
 
     cd $stage
