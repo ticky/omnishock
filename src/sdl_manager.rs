4835ad8e1b761b5304a6a57b2e24cec980e040fd
Jessica Stokes <hello@jessicastokes.net>
2018-02-16 16:58:43 -0800

    👩🏻‍⚖️ Let's move this to GPLv3

--- src/sdl_manager.rs
+++ src/sdl_manager.rs
@@ -1,3 +1,23 @@
+/*
+ * Omnishock: Something to do with game controllers!
+ * Copyright (C) 2017-2018 Jessica Stokes
+ *
+ * This file is part of Omnishock.
+ *
+ * Omnishock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Omnishock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Omnishock.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 extern crate sdl2;
 use std::collections::HashMap;
 
